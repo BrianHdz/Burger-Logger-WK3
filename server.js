c72bfb3a05ecf2f4a30040ca489f0782a4ee208d
BrianHdz <popcorn46290@sbcglobal.net>
2020-05-05 21:50:44 -0700

    New push

--- server.js
+++ server.js
@@ -12,21 +12,25 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
-
 // Setting Handlebars.
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-
-
 // Importing routes to server.
-var routes = require("./controllers/burgers_controller.js");
+// var routes = require("./controllers/burgers_controller.js");
 
-app.use(routes);
-
+// app.use(routes);
+app.get("/", function (req, res) {
+  burger.selectAll(function (data) {
+      var hbsObject = {
+          burgers: data
+      };
+      console.log(hbsObject + " controllers - Ln19");
+      res.render("index", hbsObject);
+  });
+});
 app.listen(PORT, function() {
   console.log("App now listening at localhost:" + PORT);
 });
