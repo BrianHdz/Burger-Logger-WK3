4193af7a5788886cd6be698968f6aa5d992e7ac2
BrianHdz <popcorn46290@sbcglobal.net>
2020-05-05 21:53:44 -0700

    Please Heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 var PORT = process.env.PORT || 3000;
-
+var burger = require("./models/burger.js");
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
